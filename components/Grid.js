88cec6e7a6f8a85de1c26037ec6cf7a420fa5e8f
Logan Blangenois <logan.blangenois@mediakod.com>
2020-10-01 15:51:44 +0200

    fix: update component with component name

--- components/Grid.js
+++ components/Grid.js
@@ -1,12 +1,12 @@
-import Components from './index'
-import SbEditable from 'storyblok-react'
+import Components from "./index";
+import SbEditable from "storyblok-react";
 
-export default (props) => (
+const Grid = (props) => (
   <SbEditable content={props.content}>
     <div className="grid">
-      {props.content.columns.map((blok) =>
-        Components(blok)
-      )}
+      {props.content.columns.map((blok) => Components(blok))}
     </div>
   </SbEditable>
-)
+);
+
+export default Grid;
