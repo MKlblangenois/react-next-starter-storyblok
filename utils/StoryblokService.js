c0a2bfc0edeb1748c0e5af584c322832ff19fc24
Logan Blangenois <logan.blangenois@mediakod.com>
2020-10-01 15:52:07 +0200

    build: update project source

--- utils/StoryblokService.js
+++ utils/StoryblokService.js
@@ -1,66 +1,75 @@
-import StoryblokClient from 'storyblok-js-client'
+import StoryblokClient from "storyblok-js-client";
 
 class StoryblokService {
   constructor() {
-    this.devMode = false // If true it always loads draft
-    this.token = 'vIxSxIvAUpbqD8V7NrciPAtt'
+    this.devMode = false; // If true it always loads draft
+    this.token = "oSbWAkzA0tQYZ7V4rRncgwtt";
     this.client = new StoryblokClient({
       accessToken: this.token,
       cache: {
-        clear: 'auto',
-        type: 'memory'
-      }
-    })
+        clear: "auto",
+        type: "memory",
+      },
+    });
 
-    this.query = {}
+    this.query = {};
   }
 
   getCacheVersion() {
-    return this.client.cacheVersion
+    return this.client.cacheVersion;
   }
 
   get(slug, params) {
-    params = params || {}
+    params = params || {};
 
-    if (this.getQuery('_storyblok') || this.devMode || (typeof window !== 'undefined' && window.storyblok)) {
-      params.version = 'draft'
+    if (
+      this.getQuery("_storyblok") ||
+      this.devMode ||
+      (typeof window !== "undefined" && window.storyblok)
+    ) {
+      params.version = "draft";
     }
 
-    if (typeof window !== 'undefined' && typeof window.StoryblokCacheVersion !== 'undefined') {
-      params.cv = window.StoryblokCacheVersion
+    if (
+      typeof window !== "undefined" &&
+      typeof window.StoryblokCacheVersion !== "undefined"
+    ) {
+      params.cv = window.StoryblokCacheVersion;
     }
 
-    return this.client.get(slug, params)
+    return this.client.get(slug, params);
   }
 
   initEditor(reactComponent) {
     if (window.storyblok) {
-      window.storyblok.init({accessToken: this.token})
-      window.storyblok.on(['change', 'published'], () => location.reload(true))
-      window.storyblok.on('input', (event) => {
-        if (event.story.content._uid === reactComponent.state.pageContent._uid) {
-          reactComponent.setState({pageContent: event.story.content})
+      window.storyblok.init({ accessToken: this.token });
+      window.storyblok.on(["change", "published"], () => location.reload(true));
+      window.storyblok.on("input", (event) => {
+        if (
+          event.story.content._uid === reactComponent.state.pageContent._uid
+        ) {
+          reactComponent.setState({ pageContent: event.story.content });
         }
-      })
+      });
     }
   }
 
   setQuery(query) {
-    this.query = query
+    this.query = query;
   }
 
   getQuery(param) {
-    return this.query[param]
+    return this.query[param];
   }
 
   bridge() {
-    if (!this.getQuery('_storyblok')) {
-      return ''
+    if (!this.getQuery("_storyblok")) {
+      return "";
     }
-    return (<script src='//app.storyblok.com/f/storyblok-latest.js'></script>)
+    return <script src="//app.storyblok.com/f/storyblok-latest.js"></script>;
   }
 }
 
-const storyblokInstance = new StoryblokService()
+const storyblokInstance = new StoryblokService();
 
-export default storyblokInstance
+export default storyblokInstance;
